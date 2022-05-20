c47f6bf04c233caf47a94390bf03012ac7ad4041
aminelsayed20 <aminelsayed202020@gmail.com>
2022-05-20 19:15:44 +0200

    final edit

--- src/main/java/CLient/Chat.java
+++ src/main/java/CLient/Chat.java
@@ -16,6 +16,10 @@ import static CLient.name.s;
 import static CLient.name.screen;
 import static CLient.name.str;
 import static CLient.name.str2;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.net.*;
 import java.io.*;
 import java.io.BufferedReader;
@@ -23,16 +27,16 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
 
-public class CLient extends javax.swing.JFrame implements Runnable {
+public class Chat extends javax.swing.JFrame  {
 
     public static String msg="";
    
      
-    public CLient() {
+    public Chat() {
         initComponents();
-        nameChat.setText(friendsList [friendChat]);
+        nameChat.setText("chat with " + friendsList [friendChat]);
        textArea.setText(chat[friendChat]);
-        
+
     }
 
     
@@ -40,8 +44,9 @@ public class CLient extends javax.swing.JFrame implements Runnable {
     {
          try
         {
-            str= text.getText();
-           /////       متنساش ال سكرول بانال
+           if (!text.getText().equals(""))
+           {
+                str= text.getText();
            
             String ss = str+"#"+ friendsList[friendChat];
                 dout.writeUTF(ss);
@@ -49,6 +54,7 @@ public class CLient extends javax.swing.JFrame implements Runnable {
             chat[friendChat] += "\n" + Name +" : "+str;
             textArea.setText(chat[friendChat]);
             text.setText("");
+           }
             
         }
         catch (Exception e) 
@@ -61,18 +67,24 @@ public class CLient extends javax.swing.JFrame implements Runnable {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        panel = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         textArea = new javax.swing.JTextArea();
         text = new javax.swing.JTextField();
         send = new javax.swing.JToggleButton();
         jSeparator1 = new javax.swing.JSeparator();
         nameChat = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Chat");
 
-        jPanel1.setBackground(new java.awt.Color(153, 153, 255));
+        panel.setBackground(new java.awt.Color(153, 153, 255));
 
+        textArea.setEditable(false);
         textArea.setBackground(new java.awt.Color(204, 204, 255));
         textArea.setColumns(20);
         textArea.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
@@ -82,10 +94,20 @@ public class CLient extends javax.swing.JFrame implements Runnable {
 
         text.setBackground(new java.awt.Color(204, 204, 255));
         text.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        text.setForeground(new java.awt.Color(0, 51, 51));
+        text.setForeground(new java.awt.Color(51, 51, 51));
+        text.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                textFocusGained(evt);
+            }
+        });
         text.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 textActionPerformed(evt);
+            }
+        });
+        text.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                textKeyPressed(evt);
             }
         });
 
@@ -93,9 +115,15 @@ public class CLient extends javax.swing.JFrame implements Runnable {
         send.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         send.setForeground(new java.awt.Color(102, 102, 102));
         send.setText("send");
+        send.setFocusTraversalPolicyProvider(true);
         send.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 sendActionPerformed(evt);
+            }
+        });
+        send.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                sendKeyPressed(evt);
             }
         });
 
@@ -103,51 +131,83 @@ public class CLient extends javax.swing.JFrame implements Runnable {
         nameChat.setForeground(new java.awt.Color(102, 102, 102));
         nameChat.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
+        panel.setLayout(panelLayout);
+        panelLayout.setHorizontalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(panelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelLayout.createSequentialGroup()
                         .addComponent(text, javax.swing.GroupLayout.PREFERRED_SIZE, 520, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(send, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 654, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap())))
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(panelLayout.createSequentialGroup()
                 .addGap(208, 208, 208)
                 .addComponent(nameChat, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+        panelLayout.setVerticalGroup(
+            panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelLayout.createSequentialGroup()
                 .addComponent(nameChat, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 409, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(25, 25, 25)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(text, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(send, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(25, 25, 25))
         );
 
+        jMenu1.setText("Edit");
+
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_D, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        jMenuItem1.setText("Dark mode");
+        jMenuItem1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jMenuItem1MouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jMenuItem1MouseEntered(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_DOWN_MASK));
+        jMenuItem2.setText("Normal mode");
+        jMenuItem2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jMenuItem2MouseEntered(evt);
+            }
+        });
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -162,12 +222,51 @@ public class CLient extends javax.swing.JFrame implements Runnable {
        send();
     }//GEN-LAST:event_sendActionPerformed
 
+    private void sendKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_sendKeyPressed
+       
+    }//GEN-LAST:event_sendKeyPressed
+
+    private void textKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_textKeyPressed
+                 if (evt.getKeyCode() == KeyEvent.VK_ENTER)
+                             send();
+
+    }//GEN-LAST:event_textKeyPressed
+
+    private void textFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_textFocusGained
+       
+    }//GEN-LAST:event_textFocusGained
+
+    private void jMenuItem1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem1MouseClicked
+
+    }//GEN-LAST:event_jMenuItem1MouseClicked
+
+    private void jMenuItem1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem1MouseEntered
+       
+        panel.setBackground(new java.awt.Color(51, 51, 51));
+       textArea.setBackground(new java.awt.Color(0, 0, 0));
+        textArea.setForeground(new java.awt.Color(204, 204, 204));
+        text.setBackground(new java.awt.Color(102, 102, 102));
+        text.setForeground(new java.awt.Color(204, 204, 204));
+    }//GEN-LAST:event_jMenuItem1MouseEntered
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem2MouseEntered
+        panel.setBackground(new java.awt.Color(153, 153, 255));
+         textArea.setBackground(new java.awt.Color(204, 204, 255));
+         textArea.setForeground(new java.awt.Color(51, 51, 51));
+         text.setForeground(new java.awt.Color(51, 51, 51));
+         text.setBackground(new java.awt.Color(204, 204, 255));
+    }//GEN-LAST:event_jMenuItem2MouseEntered
+
 
     public static void main(String args[]) throws Exception {
 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CLient().setVisible(true);
+                new Chat().setVisible(true);
             }     
         });
         
@@ -181,19 +280,19 @@ public class CLient extends javax.swing.JFrame implements Runnable {
 }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel nameChat;
+    private javax.swing.JPanel panel;
     private static javax.swing.JToggleButton send;
     private static javax.swing.JTextField text;
     public static javax.swing.JTextArea textArea;
     // End of variables declaration//GEN-END:variables
 
-    @Override
-    public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 
  
 }
